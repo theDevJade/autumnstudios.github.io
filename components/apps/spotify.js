27d836535afdfe8bfd0d58702ab939b4f882b47f
horizon <89808589+autumnstudios@users.noreply.github.com>
2023-10-12 13:28:08 -0700

    first commit

---
+++ components/apps/spotify.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Spotify() {
+    return (
+        <iframe src="https://embed.music.apple.com/us/playlist/do-you-ever-sleep/pl.u-d2b05ZZFLY6ALJ1" frameBorder="0" title="Apple Music" className="h-full w-full bg-ub-cool-grey"></iframe>
+    )
+}
+
+export const displaySpotify = () => {
+    <Spotify> </Spotify>
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Ubuntu from "../components/ubuntu";
+import Meta from "../components/SEO/Meta";
+
+
+
+function App() {
+  return (
+    <>
+      <Meta />
+      <Ubuntu />
+    </>
+  )
+}
+
+export default App;
