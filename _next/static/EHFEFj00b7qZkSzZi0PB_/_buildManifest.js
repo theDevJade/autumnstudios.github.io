f3af3c5d433e9499bdc291dbe2f2f140305e1b73
autumnstudios <autumnstudios@users.noreply.github.com>
2023-10-12 20:44:38 +0000

    Deploying to gh-pages from @ autumnstudios/autumnstudios.github.io@3805cf378d09b11c137f927777e65b5b8b49e86f 🚀

---
+++ _next/static/EHFEFj00b7qZkSzZi0PB_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b637e9a5-6cf11adc33c7fdd4.js","static/chunks/689-1070f70a645b4075.js","static/chunks/pages/index-086a10e1ab1d376f.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
