130536b034222cb77819a95dfaa42fbe2ce1ff36
theDevJade <89808589+theDevJade@users.noreply.github.com>
2023-10-12 19:14:12 -0700

    added year

--- components/apps/jade.js
+++ components/apps/jade.js
@@ -108,7 +108,7 @@ function About() {
             </div>
             <div className=" mt-4 md:mt-8 text-lg md:text-2xl text-center px-1">
                 <div>my name is <span className="font-bold">Jade</span> ,</div>
-                <div className="font-normal ml-1">I'm a <span className="text-pink-600 font-bold">Minecraft Kotlin Developer</span></div>
+                <div className="font-normal ml-1">I'm a <span className="text-pink-600 font-bold">Minecraft Kotlin Developer</span> of <span className="text-pink-600 font-bold">7 Years</span></div>
             </div>
             <div className=" mt-4 relative md:my-8 pt-px bg-white w-32 md:w-48">
                 <div className="bg-white absolute rounded-full p-0.5 md:p-1 top-0 transform -translate-y-1/2 left-0"></div>
